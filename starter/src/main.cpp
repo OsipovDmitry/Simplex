52a055638a078969c72bdaf5e3a7e5a93521bc0e
OsipovDmitry <dmitry1992osipov@gmail.com>
2018-06-02 13:15:03 +0300

    add assimp to starter

--- starter/src/main.cpp
+++ starter/src/main.cpp
@@ -4,11 +4,40 @@
 
 int main(int argc, char **argv)
 {
-	QApplication app(argc, argv);
+    QApplication app(argc, argv);
 
-	GLESWidget w1;
-	w1.setWindowTitle("Window 1");
-	w1.show();
+    GLESWidget w1;
+    w1.setWindowTitle("Window 1");
+    w1.show();
 
-	return app.exec();
+    return app.exec();
 }
+
+
+//class MyClass;
+//using MyClassPtr = std::shared_ptr<MyClass>;
+
+//class MyClass {
+//public:
+//    ~MyClass() {}
+//    // ...
+
+//    static void del(MyClass *p) {
+//        delete p;
+//    }
+//    static MyClassPtr generate() {
+//        MyClassPtr obj(new MyClass, del);
+//        return obj;
+//    }
+
+//protected:
+//    MyClass() {}
+
+//};
+
+//int main(int argc, char **argv)
+//{
+//    auto p1 = MyClass::generate();
+//    int i = sizeof(p1);
+//    auto p2 = MyClass::generate();
+//}
